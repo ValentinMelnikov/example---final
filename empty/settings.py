34de6f7b4e01fbd7aed4bbc32191fc35a41f2194
Valentin Melnikov <switch4xd@gmail.com>
2023-02-04 21:18:49 +0600

    add settings static

--- empty/settings.py
+++ empty/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'webapp/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'login'
